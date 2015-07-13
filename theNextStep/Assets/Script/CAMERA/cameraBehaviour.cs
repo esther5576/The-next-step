504957601daf9996a24d533ace47acff95bb2b08
esther5576 <esther.berges@hotmail.com>
2015-07-13 17:41:47 +0200

    Base Project UNITY
    basic unity project for the next step

---
+++ theNextStep/Assets/Script/CAMERA/cameraBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraBehaviour : MonoBehaviour
+{
+	public AnimationCurve _camAnimationCurve;
+
+	public float _zoomNumber;
+
+	public Animator _animation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_animation = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Zoom
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0 && _zoomNumber < 1) { // forward
+			_zoomNumber += 0.07f;
+		}
+
+		//Dezoom
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0 && _zoomNumber > 0) { // back
+			_zoomNumber -= 0.07f;
+		}
+
+		//Adjust minimum
+		if (_zoomNumber < 0) {
+			_zoomNumber = 0;
+		}
+
+		//Curve
+		float _newZoomNumber = _camAnimationCurve.Evaluate (_zoomNumber);
+
+		_animation.SetFloat ("cameraType", _newZoomNumber);
+	}
+}
---
+++ theNextStep/Assets/Script/unityAnalytics/UnityAnalyticsIntegration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Cloud.Analytics;
+
+public class UnityAnalyticsIntegration : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+		const string projectId = "0514174b-f010-4681-843a-623fd1a36555";
+		UnityAnalytics.StartSDK (projectId);
+		
+	}
+	
+}
