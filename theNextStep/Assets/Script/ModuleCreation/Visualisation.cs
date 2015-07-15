2ae1e25b4c5268fef67feeab9b4c6d503efd3053
valgoun <valgoun@gmail.com>
2015-07-15 15:34:16 +0200

    Building System & Ui
    Building System first implementation :
    - Place building (Press A)
    - First Version of the Road Construction (Press Z)
    Ui :
    - Ui for buildings

---
+++ theNextStep/Assets/Script/ModuleCreation/Visualisation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visualisation : MonoBehaviour
+{
+	public bool Collide { get { return _collide; } }
+	public GameObject ColliderObject { get { return _colliderObject; } }
+	public bool BuildAble { get; set; }
+
+
+	private GameObject _colliderObject;
+	private bool _collide;
+	private Color _initialColor;
+	// Use this for initialization
+	void Start ()
+	{
+		BuildAble = true;
+		_initialColor = GetComponent<Renderer> ().material.color;
+		GetComponent<Renderer> ().material.color = _initialColor * Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		GetComponent<Renderer> ().material.color = _initialColor * ((!_collide && BuildAble) ? Color.green : Color.red);
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.GetComponent<Terrain> ()) {
+			return;
+		}
+		Debug.Log (col.gameObject);
+		_collide = true;
+		_colliderObject = col.gameObject;
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.GetComponent<Terrain> ())
+			return;
+		_collide = false;
+		_colliderObject = null;
+	}
+
+
+}
