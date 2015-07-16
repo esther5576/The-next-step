010828c40e8518d6ccda1dff6fd86bb57f91460f
esther5576 <esther.berges@hotmail.com>
2015-07-16 10:47:33 +0200

    Must have buildings with stats needs and production
    Food, solar panel, human habitats, water extraction center, main
    building, landing zone.

--- theNextStep/Assets/Scripts/Buildings/ProductionAndNeeds.cs
+++ theNextStep/Assets/Scripts/Buildings/ProductionAndNeeds.cs
@@ -9,6 +9,7 @@ public class ProductionAndNeeds : MonoBehaviour
 	public float _CO2NeedsPerDay = 0;
 	public float _O2NeedsPerDay = 0;
 	public float _mineralsNeedsPerDay = 0;
+	public float _foodNeedsPerDay = 0;
 	public float _HumansNeeds = 0;
 	#endregion
 
@@ -18,6 +19,7 @@ public class ProductionAndNeeds : MonoBehaviour
 	public float _CO2ProductionPerDay = 0;
 	public float _O2ProductionPerDay = 0;
 	public float _mineralsProductionPerDay = 0;
+	public float _foodProductionPerDay = 0;
 	public float _HumansProduction = 0;
 	#endregion
 
@@ -56,17 +58,21 @@ public class ProductionAndNeeds : MonoBehaviour
 		if (_mineralsNeedsPerDay != 0) {
 			Camera.main.GetComponent<gameStats> ()._actualMineral -= ((_mineralsNeedsPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}
+
+		if (_foodNeedsPerDay != 0) {
+			Camera.main.GetComponent<gameStats> ()._actualFood -= ((_foodNeedsPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
+		}
 	}
 	#endregion
 
 	#region Production
 	void production ()
 	{
-		if (_waterNeedsPerDay != 0) {
+		if (_waterProductionPerDay != 0) {
 			Camera.main.GetComponent<gameStats> ()._actualWater += ((_waterProductionPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}
 		
-		if (_electricityNeedsPerDay != 0) {
+		if (_electricityProductionPerDay != 0) {
 			Camera.main.GetComponent<gameStats> ()._actualElectricity += ((_electricityProductionPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}
 		
@@ -74,12 +80,16 @@ public class ProductionAndNeeds : MonoBehaviour
 			//Camera.main.GetComponent<gameStats> (). -= ((_CO2NeedsPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}*/
 		
-		if (_O2NeedsPerDay != 0) {
+		if (_O2ProductionPerDay != 0) {
 			Camera.main.GetComponent<gameStats> ()._actualOxygen += ((_O2ProductionPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}
 		
-		if (_mineralsNeedsPerDay != 0) {
+		if (_mineralsProductionPerDay != 0) {
 			Camera.main.GetComponent<gameStats> ()._actualMineral += ((_mineralsProductionPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
+		}
+
+		if (_foodProductionPerDay != 0) {
+			Camera.main.GetComponent<gameStats> ()._actualFood += ((_foodProductionPerDay / (this.GetComponent<dayToDayCounter> ()._dayLength * 2)) * Time.deltaTime);
 		}
 	}
 	#endregion
