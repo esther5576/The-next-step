f52dabb9916ba69b17338f902070c974d4f07929
esther5576 <esther.berges@hotmail.com>
2015-07-24 16:36:23 +0200

    rangement des assets + v1rover mecanic

---
+++ theNextStep/Assets/Scripts/Buildings/MapRover.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapRover : MonoBehaviour
+{
+	[HideInInspector]
+	public GameObject
+		_roverCanvas;
+	[HideInInspector]
+	public GameObject
+		_zonesCanvas;
+	[HideInInspector]
+	public GameObject
+		_nextPartCanvas;
+	[HideInInspector]
+	public int
+		_zone;
+	// Use this for initialization
+	void Start ()
+	{
+		_roverCanvas = GameObject.Find ("RoverStationCanvas");
+		_zonesCanvas = GameObject.Find ("Zones");
+		_nextPartCanvas = GameObject.Find ("NextPart");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void openRoverMap (bool _openMap)
+	{
+		_roverCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+		_roverCanvas.GetComponent<CanvasGroup> ().interactable = false;
+
+		this.GetComponent<CanvasGroup> ().alpha = 1;
+		this.GetComponent<CanvasGroup> ().interactable = true;
+
+		_zonesCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+		_zonesCanvas.GetComponent<CanvasGroup> ().interactable = true;
+		
+		_nextPartCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+		_nextPartCanvas.GetComponent<CanvasGroup> ().interactable = false;
+	}
+
+	public void topLeftPartOfTheMap (bool _openZone1)
+	{
+		canvasGroupChange ();
+		_zone = 1;
+	}
+
+	public void topRightPartOfTheMap (bool _openZone2)
+	{
+		canvasGroupChange ();
+		_zone = 2;
+	}
+
+	public void bottomLeftPartOfTheMap (bool _openZone3)
+	{
+		canvasGroupChange ();
+		_zone = 3;
+	}
+
+	public void bottomRightPartOfTheMap (bool _openZone4)
+	{
+		canvasGroupChange ();
+		_zone = 4;
+	}
+
+	void canvasGroupChange ()
+	{
+		_zonesCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+		_zonesCanvas.GetComponent<CanvasGroup> ().interactable = false;
+		
+		_nextPartCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+		_nextPartCanvas.GetComponent<CanvasGroup> ().interactable = true;
+	}
+}
--- theNextStep/Assets/Scripts/Buildings/roverStation.cs
+++ theNextStep/Assets/Scripts/Buildings/roverStation.cs
@@ -4,37 +4,57 @@ using System.Collections;
 public class roverStation : MonoBehaviour
 {
 	public GameObject _roverCanvas;
+	public GameObject _sliderTime;
+	public GameObject _mapCanvas;
+	public GameObject _zonesCanvas;
+	public GameObject _nextPartCanvas;
+
+	public bool _roverOut;
 	// Use this for initialization
 	void Start ()
 	{
 		_roverCanvas = GameObject.Find ("RoverStationCanvas");
+		_sliderTime = GameObject.Find ("SlidderForSpeed");
+		_mapCanvas = GameObject.Find ("MapRoverCanvas");
+		_zonesCanvas = GameObject.Find ("Zones");
+		_nextPartCanvas = GameObject.Find ("NextPart");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
-		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.rigidbody != null && (hit.transform.name == "RoverStation(Clone)" || hit.transform.name == "RoverStation")) {
-					_roverCanvas.GetComponent<CanvasGroup> ().alpha = 1;
-					_roverCanvas.GetComponent<CanvasGroup> ().interactable = true;
+		if (_roverOut == false) {
+			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit)) {
+					if (hit.rigidbody != null && (hit.transform.name == "RoverStation(Clone)" || hit.transform.name == "RoverStation")) {
+						_roverCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+						_roverCanvas.GetComponent<CanvasGroup> ().interactable = true;
+						_sliderTime.GetComponent<slowAndFastMotion> ()._speed = 0;
+						_sliderTime.GetComponent<CanvasGroup> ().alpha = 0;
+						_sliderTime.GetComponent<CanvasGroup> ().interactable = false;
+					}
 				}
 			}
 		}
+
 		if (Input.GetMouseButtonDown (1)) {
 			_roverCanvas.GetComponent<CanvasGroup> ().alpha = 0;
 			_roverCanvas.GetComponent<CanvasGroup> ().interactable = false;
-			Time.timeScale = 1;
-			Camera.main.GetComponent<DayNightController> ().timeMultiplier = 1;
+			_sliderTime.GetComponent<slowAndFastMotion> ()._speed = 1;
+			_sliderTime.GetComponent<CanvasGroup> ().alpha = 1;
+			_sliderTime.GetComponent<CanvasGroup> ().interactable = true;
+
+			_mapCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+			_mapCanvas.GetComponent<CanvasGroup> ().interactable = false;
+
+			_zonesCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+			_zonesCanvas.GetComponent<CanvasGroup> ().interactable = true;
+
+			_nextPartCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+			_nextPartCanvas.GetComponent<CanvasGroup> ().interactable = false;
 		}
 	}
 
-	public void openRoverMap (bool _openMap)
-	{
-		//Time.timeScale = 0;
-		Camera.main.GetComponent<DayNightController> ().timeMultiplier = 0.1f;
-	}
 }
