315e61ce8f393f4d0f847e76237db8f661a84313
esther5576 <esther.berges@hotmail.com>
2015-07-29 14:25:55 +0200

    Instructions canvas et tower com canvas

---
+++ theNextStep/Assets/Scripts/InstructionsScripts.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsScripts : MonoBehaviour
+{
+	public GameObject _slidderSpeedCanvas;
+	public GameObject _ConstructionCanvas;
+	// Use this for initialization
+	void Start ()
+	{
+		_slidderSpeedCanvas = GameObject.Find ("SlidderForSpeed");
+		_ConstructionCanvas = GameObject.Find ("Construction");
+
+		_slidderSpeedCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+		_slidderSpeedCanvas.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		_slidderSpeedCanvas.GetComponent<CanvasGroup> ().interactable = false;
+		
+		_ConstructionCanvas.GetComponent<CanvasGroup> ().alpha = 0;
+		_ConstructionCanvas.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		_ConstructionCanvas.GetComponent<CanvasGroup> ().interactable = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (Input.GetMouseButtonDown (1)) {
+			this.GetComponent<CanvasGroup> ().alpha = 0;
+			this.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+			this.GetComponent<CanvasGroup> ().interactable = false;
+
+			_slidderSpeedCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+			_slidderSpeedCanvas.GetComponent<CanvasGroup> ().blocksRaycasts = true;
+			_slidderSpeedCanvas.GetComponent<CanvasGroup> ().interactable = true;
+
+			_ConstructionCanvas.GetComponent<CanvasGroup> ().alpha = 1;
+			_ConstructionCanvas.GetComponent<CanvasGroup> ().blocksRaycasts = true;
+			_ConstructionCanvas.GetComponent<CanvasGroup> ().interactable = true;
+		}
+
+	}
+}
